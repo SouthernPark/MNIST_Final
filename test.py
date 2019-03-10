182e752968e7ac98ec2c8bb02b327cf49f3d9f94
Southern_park <2848919007@qq.com>
2019-03-10 20:26:44 +0800

    该python文件将会利用SAVE文件夹当中的模型来识别用户的手写体图片

---
+++ test.py
@@ -0,0 +1,108 @@
+﻿from PIL import Image
+import tensorflow as tf
+
+
+def imageprepare(file_name):
+    """
+    This function returns the pixel values.
+    The imput is a png file location.
+    """
+    #in terminal 'mogrify -format png *.jpg' convert jpg to png
+    im = Image.open(file_name)
+
+    im = im.convert('L')
+
+    #im.save("C:/Users/mechrevo/Desktop/sample.png")
+    
+    
+    tv = list(im.getdata()) #get pixel values
+
+    #normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
+    tva = [ (255-x)*1.0/255.0 for x in tv]
+    return tva
+
+
+
+    """
+    This function returns the predicted integer.
+    The imput is the pixel values from the imageprepare() function.
+    """
+
+    # Define the model (same as when creating the model file)
+
+
+
+
+
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape,stddev = 0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1,shape = shape)
+    return tf.Variable(initial)
+
+def conv2d(x,W):
+    return tf.nn.conv2d(x, W, strides = [1,1,1,1], padding = 'SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+def get_y_conv():
+
+    x = tf.placeholder(tf.float32, [None, 784])
+
+    y_ = tf.placeholder(tf.float32, [None, 10])
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+
+    x_image = tf.reshape(x,[-1,28,28,1])
+
+    h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    keep_prob = tf.placeholder("float")
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+
+    y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+    return y_conv, keep_prob, h_conv2, y_, x
+def classify(location):
+    result = imageprepare(location)
+    y_conv, keep_prob, h_conv2, y_, x = get_y_conv()
+    cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess, "/app/SAVE/model.ckpt")#这里使用了之前保存的模型参数
+
+
+        prediction=tf.argmax(y_conv,1)
+        predint=prediction.eval(feed_dict={x: [result], keep_prob: 1.0}, session=sess)
+        print(h_conv2)
+
+        print('识别结果:')
+        print(predint[0])
+
+        return predint[0]
